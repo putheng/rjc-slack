851ff265481accea98618169afef788c9320cf3a
Putheng <puthengemail@gmail.com>
2018-09-20 08:41:36 +0000

    add count function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $count = file_get_contents('count.txt') + 1;
-        file_put_contents('count.txt', $count);
+        return response('ok', 200);
     }
 }
