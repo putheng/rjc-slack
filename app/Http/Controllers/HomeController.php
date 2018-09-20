4784a0fd65a89c391617f6601518c5cd657a3913
Putheng <puthengemail@gmail.com>
2018-09-20 09:15:21 +0000

    add count function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        dd($request);
+        
         $count = file_get_contents('count.txt') + 1;
         file_put_contents('count.txt', $count);
         
