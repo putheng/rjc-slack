85fd6aac7fc240277044b71f57737689bd935da9
Putheng <puthengemail@gmail.com>
2018-09-24 03:53:38 +0000

    add ifram video

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,1 +1,8 @@
-<iframe src="https://drive.google.com/file/d/1dB_bzPrrLyKbnH7peBV2rY77p9_83WQP/preview" width="640" height="480"></iframe>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+<button id="play" style="margin-top: 300px;">Play</button>
+
+<video id="video" width="320" height="240" controls>
+   <source src="https://drive.google.com/uc?export=download&id=1dB_bzPrrLyKbnH7peBV2rY77p9_83WQP" type='video/mp4'>
+   Your browser does not support the video tag.
+</video> 
