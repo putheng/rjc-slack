2243b6abf68ad15223e41d5779a10437b0569a4f
Putheng <puthengemail@gmail.com>
2018-10-18 08:10:42 +0000

    add slack request

--- app/Http/Controllers/RequestLeaveController.php
+++ app/Http/Controllers/RequestLeaveController.php
@@ -8,8 +8,6 @@ class RequestLeaveController extends Controller
 {
     public function index(Request $request)
     {
-        return $request->all();
-        
-        //file_put_contents('count.txt', $request->all()->toJson());
+        file_put_contents('count.txt', $request->all());
     }
 }
