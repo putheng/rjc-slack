53177f32d3479201b3fb20819281331c4b711bb8
Putheng <puthengemail@gmail.com>
2018-10-22 09:28:07 +0000

    update hook

--- app/Http/Controllers/RequestLeaveController.php
+++ app/Http/Controllers/RequestLeaveController.php
@@ -8,6 +8,6 @@ class RequestLeaveController extends Controller
 {
     public function index(Request $request)
     {
-        file_put_contents('count.txt', json_encode($request->all()));
+        file_put_contents('count.txt', json_encode($request->payload));
     }
 }
