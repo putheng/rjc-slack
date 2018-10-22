03b79e8b8cc932e9205cd0e3df1b777b880003eb
Putheng <puthengemail@gmail.com>
2018-10-22 07:42:06 +0000

    update

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -53,7 +53,7 @@ class ApprovalController extends Controller
                                     {
                                         "type": "button",
                                         "text": "Submit Your Form",
-                                        "url": "http://renet-slack.herokuapp.com/slack/approval/form?token="'. str_random(60) .',
+                                        "url": "http://renet-slack.herokuapp.com/slack/approval/form?token="'. $id .',
                                         "style": "primary"
                                     }
                                 ]
