69f9f2bd276112da7106ccb97e65c542964934bc
Putheng <puthengemail@gmail.com>
2018-10-25 03:52:16 +0000

    update ApprovalController

--- app/Http/Controllers/RequestLeaveController.php
+++ app/Http/Controllers/RequestLeaveController.php
@@ -10,6 +10,6 @@ class RequestLeaveController extends Controller
     {
         $payload = json_decode($request->payload);
         
-        file_put_contents('count.txt', json_encode($payload->actions[0]->value));
+        file_put_contents('count.txt', $payload->actions[0]->value);
     }
 }
