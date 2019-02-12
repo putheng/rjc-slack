ad29bd34854034f257a7e2c001120b02acc241fa
Putheng <puthengemail@gmail.com>
2019-02-12 08:19:02 +0000

    update approval approver

--- app/Http/Controllers/ApprovalController.php
+++ app/Http/Controllers/ApprovalController.php
@@ -163,11 +163,11 @@ class ApprovalController extends Controller
             $approver = Approver::where('slackid', $userid)->first();
 
             $aa = new ApprovalApprover;
-            $aa->approval_id = $approval->id;
+            $aa->approval_id = $requestid;
             $aa->approver_id = $approver->id;
             $aa->save();
             
-            //$this->sendApprovedRequest($userid, $approval);
+            $this->sendApprovedRequest($userid, $approval);
             
         }
 
