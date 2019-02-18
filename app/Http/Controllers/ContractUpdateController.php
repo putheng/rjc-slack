fec6c1a59098087cf2c460f68a5405d89c059426
Putheng <puthengemail@gmail.com>
2019-02-18 04:14:56 +0000

    update

--- app/Http/Controllers/ContractUpdateController.php
+++ app/Http/Controllers/ContractUpdateController.php
@@ -15,7 +15,8 @@ class ContractUpdateController extends Controller
 
     public function slackAuthorize()
 	{
-		return 'https://hooks.slack.com/services/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';
+		return 'https://hooks.slack.com/services/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';
+		//return 'https://hooks.slack.com/services/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';
 	}
 
     public function index()
